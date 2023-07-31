2c455292c66aaacd5f2376ff93f6c0d1d1fb0e1d
Nemechix <nemesi.jobs@gmail.com>
2023-07-31 12:46:34 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import WeatherApp from "./components/weatherapp";
+
+
+function App() {
+  return (
+    <>
+    <WeatherApp></WeatherApp>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/weatherapp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WeatherApp() {
+  return (
+    <div>Hola</div>
+  )
+}
+
+export default WeatherApp
