fa8394d126ef62953adc8a2c7f0625c572c63e9d
Nemechix <nemesi.jobs@gmail.com>
2023-07-31 14:38:22 +0000

    Iframe googleMaps Added, Components: weatherapp, weatherForm, weatherMainInfo done

--- src/components/weatherapp.js
+++ src/components/weatherapp.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import WeatherForm from './weatherForm'
+import WeatherMainInfo from './weatherMainInfo'
 
 function WeatherApp() {
+    const [weather, setWeather] = useState(null)
+
+    useEffect(()=> {
+        loadInfo()
+    }, [])
+
+    useEffect(()=> {
+        document.title = `Weather | ${weather?.location.name ?? ""}`
+    }, [weather])
+
+    async function loadInfo(city = "london"){
+        try{
+            const request = await fetch(`${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`)
+            const json = await request.json()
+            setWeather(json)
+        } catch(error){
+
+        }
+    }
+    
+    function handleChangeCity(city){
+        setWeather(null)
+        loadInfo(city)
+
+    }
   return (
-    <div>Hola</div>
+    <div>
+        <WeatherForm onChangeCity={handleChangeCity}/>
+        <WeatherMainInfo weather={weather}/>
+    </div>
   )
 }
 
